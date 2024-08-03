cc0e3223c4e6c107f24d95c0bcbd5fe164b78e54
AxelanO7 <axel@btwedutech.com>
2024-08-04 01:56:05 +0800

    chore: Update site name and description

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Jeremia Axelano",
-  description: "My personal website",
+  name: "Axelano",
+  description: "Portfolio website of Axelano",
   navItems: [
     {
       label: "About",
@@ -31,11 +31,4 @@ export const siteConfig = {
       href: "projects",
     },
   ],
-  links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
