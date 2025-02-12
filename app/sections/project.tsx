6ed705a0d574797d7a63580e0c9e82ca6ff1cd07
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-12 17:33:13 +0800

    ✨ feat(project): add jobseeker apps to project section
    - add Jobseeker .app and Jobseeker .partners projects
    - include descriptions, images, and tech stack information
    - update project links and image imports
    
    ♻️ refactor(project): use link prop instead of github
    
    - replace github prop with link for project URLs
    - update project data to use link instead of github

--- app/sections/project.tsx
+++ app/sections/project.tsx
@@ -10,6 +10,8 @@ import imgBnShop from "@/public/bn_shop.png";
 import imgVillaManis from "@/public/villa_manis.png";
 import imgSmartBtw from "@/public/smartbtw.png";
 import imgBtwEdutech from "@/public/btwedutech.png";
+import imgJobseerApp from "@/public/jobseeker_app.png";
+import imgJobseerPartners from "@/public/jobseeker_partners.png";
 import Image from "next/image";
 
 export default function ProjectSection() {
@@ -22,7 +24,7 @@ export default function ProjectSection() {
         "This information system is designed to effectively manage and analyze administrative data pertaining to the population of Bulian Village within Kubutambahan District. The system aims to provide a comprehensive and up-to-date database of demographic information, facilitating efficient governance, policymaking, and service delivery.",
       image: imgSiapenku,
       techStack: ["Laravel", "Vue"],
-      github: "http://siapenku.stion.site",
+      link: "http://siapenku.stion.site",
       demo: "",
     },
     {
@@ -33,7 +35,7 @@ export default function ProjectSection() {
         "This project aims to develop a comprehensive online platform dedicated to the digitization and preservation of temple shrines. By leveraging advanced 3D modeling and imaging technologies, we will create an immersive virtual environment that allows users to explore and interact with these sacred spaces in unprecedented detail.",
       image: imgSigapura,
       techStack: ["PHP"],
-      github: "https://github.com/AxelanO7/sigapura-web-php",
+      link: "https://github.com/AxelanO7/sigapura-web-php",
       demo: "",
     },
     {
@@ -44,7 +46,7 @@ export default function ProjectSection() {
         "This project entails the development of a comprehensive online platform designed to streamline and enhance interactions within a campus community. By categorizing and organizing campus activities, the website will serve as a centralized hub for students to discover and engage with diverse opportunities, clubs, and events. This platform aims to foster a stronger sense of community, facilitate collaboration between students and organizations, and optimize the overall campus experience.",
       image: imgKoi,
       techStack: ["React", "Go"],
-      github: "https://github.com/AxelanO7/koi-frontend-web-js",
+      link: "https://github.com/AxelanO7/koi-frontend-web-js",
       demo: "",
     },
     {
@@ -55,7 +57,7 @@ export default function ProjectSection() {
         "This web-based application streamlines inventory management by providing real-time tracking of incoming and outgoing goods, automating processes like reorder points and inventory adjustments, generating accurate reports, and improving inventory accuracy and visibility. By optimizing these aspects, businesses can enhance efficiency, reduce costs, and make informed decisions.",
       image: imgBnShop,
       techStack: ["React", "Go"],
-      github: "https://github.com/AxelanO7/bn-shop-frontend-web-js",
+      link: "https://github.com/AxelanO7/bn-shop-frontend-web-js",
       demo: "",
     },
     {
@@ -66,7 +68,7 @@ export default function ProjectSection() {
         "This web-based application streamlines financial and accounting processes by providing comprehensive reporting, efficient data management, real-time analytics, compliance management, and integration capabilities. By automating and optimizing these aspects, businesses can improve efficiency, enhance decision-making, and ensure financial accuracy and compliance.",
       image: imgVillaManis,
       techStack: ["React", "Go"],
-      github: "https://github.com/AxelanO7/villa-manis-frontend-web-js",
+      link: "https://github.com/AxelanO7/villa-manis-frontend-web-js",
       demo: "",
     },
     {
@@ -77,7 +79,7 @@ export default function ProjectSection() {
         "This web-based application streamlines inventory management by providing real-time tracking, automated reordering, accurate reporting, and tools to improve inventory accuracy and visibility. By automating and optimizing these aspects, businesses can enhance efficiency, reduce costs, and make informed decisions.",
       image: imgAssyarif,
       techStack: ["React", "Go"],
-      github: "https://github.com/AxelanO7/assyarif-frontend-web-js",
+      link: "https://github.com/AxelanO7/assyarif-frontend-web-js",
       demo: "",
     },
     {
@@ -88,7 +90,7 @@ export default function ProjectSection() {
         "This project aims to develop a highly adaptable calculator application capable of seamlessly adjusting its interface to accommodate diverse screen sizes, from compact smartphones to expansive tablets. To ensure data privacy and security, the app will incorporate robust user authentication mechanisms. Furthermore, it will be designed to dynamically fetch external data, such as real-time financial information or scientific constants, enhancing its utility and providing users with access to up-to-date calculations and computations.",
       image: imgCalculator,
       techStack: ["Flutter"],
-      github: "https://github.com/AxelanO7/live-code-hybrid-flutter",
+      link: "https://github.com/AxelanO7/live-code-hybrid-flutter",
       demo: "",
     },
     {
@@ -99,7 +101,7 @@ export default function ProjectSection() {
         "This mobile application provides a realistic simulation experience for individuals preparing for job tryouts. By offering interactive exercises, assessments, and personalized feedback, the app helps users develop essential skills, familiarize themselves with the job environment, build confidence, and increase their chances of success in job interviews and tryouts.",
       image: imgSmartBtw,
       techStack: ["Flutter"],
-      github: "",
+      link: "",
       demo: "",
     },
     {
@@ -110,7 +112,29 @@ export default function ProjectSection() {
         "This mobile application provides a realistic simulation experience for individuals preparing for job tryouts. By offering interactive exercises, assessments, and personalized feedback, the app helps users develop essential skills, familiarize themselves with the job environment, build confidence, and increase their chances of success in job interviews and tryouts.",
       image: imgBtwEdutech,
       techStack: ["Flutter"],
-      github: "",
+      link: "",
+      demo: "",
+    },
+    {
+      name: "Jobseeker .app",
+      shortDescription:
+        "Mobile app for job seekers: Simplified job search, skill-based matching, and quick job opportunities.",
+      description:
+        "This project aims to develop a user-friendly mobile application tailored for job seekers, offering a seamless and efficient job search experience. The app will feature a social media-like interface, allowing users to browse and apply for jobs effortlessly. Key functionalities include skill-based job matching, quick access to job opportunities, and a streamlined process to connect users with potential employers. The app eliminates the need for long queues and complex processes, making job hunting as simple as scrolling through social media.",
+      image: imgJobseerApp,
+      techStack: ["Flutter"],
+      link: "https://play.google.com/store/apps/details?id=com.jobseeker.app&hl=id",
+      demo: "",
+    },
+    {
+      name: "Jobseeker .partners",
+      shortDescription:
+        "Mobile app for job seekers: Easy job search, skill matching, fast job opportunities.",
+      description:
+        "This project creates a user-friendly mobile app for job seekers, simplifying the job search.  It features a social media-like interface for easy browsing and application, skill-based matching to connect job seekers with relevant opportunities, quick access to available jobs, and a streamlined process to connect with employers.  The app makes job hunting as simple as scrolling through social media, eliminating complex processes.",
+      image: imgJobseerPartners,
+      techStack: ["Flutter"],
+      link: "https://play.google.com/store/apps/details?id=com.jobseeker.partners&hl=id",
       demo: "",
     },
     {
@@ -121,7 +145,7 @@ export default function ProjectSection() {
         "This project aims to develop a mobile application designed to introduce and explore the vast spectrum of colors. The app will serve as an interactive and engaging platform for users of all ages to learn about color theory, identification, and appreciation.",
       image: imgColorLearning,
       techStack: ["Java"],
-      github: "https://github.com/AxelanO7/color-learning-mobile-java",
+      link: "https://github.com/AxelanO7/color-learning-mobile-java",
       demo: "",
     },
     {
@@ -144,7 +168,7 @@ export default function ProjectSection() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-auto transition-all duration-300 border-2 border-teal-500 rounded-lg p-4">
         {projects.map((project, index) => (
           <Card key={index} className="hover:scale-105 transform">
-            <a href={project.github} target="_blank" rel="noreferrer noopener">
+            <a href={project.link} target="_blank" rel="noreferrer noopener">
               <Image
                 src={project.image || ""}
                 alt={project.name}
