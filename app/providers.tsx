3cd1d79fc05646b9df1b5986c3a5c552906c04e5
AxelanO7 <jeremia123.jm@gmail.com>
2025-11-23 18:31:15 +0800

    ♻️ refactor(providers): rename NextUIProvider to HeroUIProvider
    - rename NextUIProvider to HeroUIProvider to align with project name

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { NextUIProvider } from "@heroui/react";
+import { HeroUIProvider } from "@heroui/react";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
@@ -16,12 +16,12 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
+    <HeroUIProvider navigate={router.push}>
       <NextThemesProvider {...themeProps}>
         <LazyMotion features={domAnimation} strict>
           <MotionConfig reducedMotion="user">{children}</MotionConfig>
         </LazyMotion>
       </NextThemesProvider>
-    </NextUIProvider>
+    </HeroUIProvider>
   );
 }
